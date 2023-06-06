7d838b5b49952b2f3d092f42757cd81e7f2551fe
Laboratório de Informática <a27.3@facens.onmicrosoft.com>
2023-06-05 22:37:20 -0300

    Update While

--- Program.cs
+++ Program.cs
@@ -2,3 +2,29 @@
 using System.Globalization;
 class Program
 {
+    static void Main()
+    {
+        bool repeat = true;
+
+        while (repeat)
+        {
+
+            int num, i = 1;
+
+            Console.WriteLine("Digite um número positivo inteiro: ");
+            num = int.Parse(Console.ReadLine());
+
+
+            while (i <= num)
+            {
+                Console.Write(i + " ");
+                i++;
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Deseja repetir o processo? (S/N): ");
+            string repeatInput = Console.ReadLine();
+            repeat = string.Equals(repeatInput, "S", StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
