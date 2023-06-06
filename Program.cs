0407e3385377ddbf15f15a2f1b18e9a9d5c7612e
Dennis <126710731+GatoEstunado@users.noreply.github.com>
2023-06-06 14:06:30 -0300

    Update Do_While

--- Program.cs
+++ Program.cs
@@ -1,36 +1,53 @@
-﻿using System;
+// Bibliotecas
+using System;
 using System.Globalization;
+
+// Classe Program
 class Program
 {
+    // Programa pricipal
     static void Main()
     {
+        // Booleano para o looping
         bool repeat = true;
-
         while (repeat)
         {
+            
+        // Váriaveis
         int num;
         bool Invalid = false;
 
+        // DO, para executar os seguintes comando enquanto cumprir seus requisitos
         do
         {
-            Console.WriteLine("Digite um número de 1 a 10: ");
+            // Interface do usuário para inserir um número
+            Console.WriteLine("Digite um numero de 1 a 10: ");
+            // Ler e receber os valores
             string input = Console.ReadLine();
 
+            // IF para que se o número dado for menor que 1 e maior que 10, dar a mensagem
             if (!int.TryParse(input, out num) || num < 1 || num > 10)
             {
-                Console.WriteLine("Número inválido. Tente novamente, com valores de 1 a 10.");
+                // Interface de número inválido
+                Console.WriteLine("Numero invalido. Tente novamente, com valores de 1 a 10.");
             }
+            // ELSE para caso o número seja válido, prossiga
             else
             {
                 Invalid = true;
             }
+        }
+        // WHILE, Enquanto cumprir os requisitos (true)
+        while (!Invalid);
+        // Interface do usuário
+        Console.WriteLine("Voce digitou: " + num + ".");
 
-        } while (!Invalid);
-        Console.WriteLine("Você digitou: " + num + ".");
-
-        Console.WriteLine("Deseja digitar um novo número? (S/N): ");
+        // Loop para repetir o processo
+        Console.WriteLine("Deseja digitar um novo numero? (S/N): ");
+        // Receber a resposta
         string repeatInput = Console.ReadLine();
 
+        // Se a resposta for igual a 'S' ou 's', repetir
         repeat = string.Equals(repeatInput, "S", StringComparison.OrdinalIgnoreCase);
         }
     }
